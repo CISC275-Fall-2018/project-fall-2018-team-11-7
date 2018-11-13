de83c5db5f35fbde15fbfe00b5b501fed2570e04
Hallie Fausey <hfausey@udel.edu>
2018-11-12 19:01:15 -0500

    first version of Animal and Direction classes

--- Animal.java
+++ Animal.java
@@ -2,17 +2,45 @@
 public abstract class Animal {
 	int xloc;
 	int yloc;
+	int xvel;
+	int yvel;
 	int imageWidth;
 	int imageHeight;
 	Direction d;
 	
-	int[] getPositions() {
-		return null;
+	public int getX() {
+		return xloc;
 	}
-	Direction[] getDirections() {
-		return null;
+	public int getY() {
+		return yloc;
 	}
-	void updatePositions() {}
+	Direction getDirection() {
+		return d;
+	}
+	void updatePositions() {
+		switch (d.getName()) {
+			case "north":
+				yloc +=yvel;
+			case "northeast":
+				xloc+=xvel;
+				yloc+=yvel;
+			case "east":
+				xloc+=xvel;
+			case "southeast":
+				xloc+=xvel;
+				yloc-=yvel;
+			case "south":
+				yloc-=0;
+			case "southwest":
+				xloc-=xvel;
+				yloc-=yvel;
+			case "west":
+				xloc -=xvel;
+			case "northwest":
+				xloc -= xvel;
+				yloc +=yvel;
+		}
+	}
 	
 	
 }
